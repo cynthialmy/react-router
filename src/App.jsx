4738511463fa8657d84bdd8092ecca2d70db4011
Mengyuan Li <mli114@volvocars.com>
2024-01-02 13:41:53 +0100

    added page content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home.jsx";
-import About from "./About.jsx";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages//Home.jsx";
+import About from "./pages/About.jsx";
 
 function App() {
 	return (
 		<BrowserRouter>
+			<header>
+				<Link className="site-logo" to="/">
+					#VANLIFE
+				</Link>
+				<nav>
+					<Link to="/about">About</Link>
+					<Link to="/vans">Vans</Link>
+				</nav>
+			</header>
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
